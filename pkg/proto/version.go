51ec0810e478f65151e5a1f704c7eef03ec23058
deadprogram <ron@hybridgroup.com>
2025-06-10 09:04:30 +0200

    feature: add Version() function to proto package
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ pkg/proto/version.go
@@ -0,0 +1,10 @@
+package proto
+
+// Current version of the proto package.
+// Update this version for each release.
+const version = "0.1.0"
+
+// Version returns the current version of the proto package.
+func Version() string {
+	return version
+}
